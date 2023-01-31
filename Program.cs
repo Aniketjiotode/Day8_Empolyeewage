63a4eee8a036f3b88b573e0b5fc4d71d35b71cbc
Aniketjiotode <aniketjiotode07@gmail.com>
2023-01-31 17:13:20 +0530

    Added: UC1 changes

--- Program.cs
+++ Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 
-namespace Day8_Empolyeewage
+namespace Empolyee_Wage
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome To Employee Wage App");
+            Console.WriteLine("Enter Employee Name: ");
+            string name = Console.ReadLine();
+            Random random = new Random();
+            int isPresent = random.Next(0, 2);
+
+            if (isPresent == 0)
+            {
+                Console.WriteLine(name + " is Absent");
+            }
+            else
+            {
+                Console.WriteLine(name + " is Present" );
+            }
         }
     }
 }
