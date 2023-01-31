2b8098832b8490a44dcd03b1232a54700faba784
Aniketjiotode <aniketjiotode07@gmail.com>
2023-01-31 18:50:05 +0530

    Added: UC7 changes

---
+++ Empolyee.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Empolyee_Wage
+{
+     public class Empolyee
+    {
+            int isPresent;
+            int dayHour = 8;
+            int wagePerHour = 20;
+            int partimeHour = 4;
+            int dayPerMonth = 20;
+            public  int monthlyWage;
+            public string name;
+
+        public Empolyee()
+        {
+
+
+            Console.WriteLine("Welcome To Employee Wage App");
+            Console.WriteLine("Enter Employee Name: ");
+             name = Console.ReadLine();
+        }
+
+            public int wage()
+            {
+
+                Random random = new Random();
+                int totalHour = 0;
+                int i = 0;
+                while (totalHour <= 100 && i <= dayPerMonth)
+                {
+                    isPresent = random.Next(0, 3);
+                    switch (isPresent)
+                    {
+                        case 0:
+                            totalHour += 0;
+                            break;
+
+
+                        case 1:
+                            totalHour += dayHour;
+                            break;
+
+
+                        case 2:
+                            totalHour += partimeHour;
+                           break;
+                    }
+
+                    i++;
+                }
+
+               return  monthlyWage = totalHour * wagePerHour;
+                
+            }
+
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,50 +2,14 @@
 
 namespace Empolyee_Wage
 {
-    internal class Program
+    public class Program
     {
+                     
         static void Main(string[] args)
         {
-
-            Console.WriteLine("Welcome To Employee Wage App");
-            Console.WriteLine("Enter Employee Name: ");
-            string name = Console.ReadLine();
-            Random random = new Random();
-            int isPresent = random.Next(0, 3);
-            int dayHour = 8;
-            int wagePerHour = 20;
-            int partimeHour = 4;
-            int dayPerMonth = 20;
-            int totalHour = 0;
-            int i = 0;
-
-            while (totalHour <= 100 && i <= dayPerMonth)
-            {
-
-                switch (isPresent)
-                {
-                    case 0:
-                        totalHour += 0;
-                        break;
-
-
-                    case 1:
-                        totalHour += dayHour;
-                        break;
-
-
-                    case 2:
-                        totalHour += partimeHour;
-                        break;
-
-                }
-
-                i++;
-
-            }
-
-            int monthlyWage = totalHour * wagePerHour;
-            Console.WriteLine($"{name} Monthly wage= {monthlyWage}");
+           
+            Empolyee EmpolyeeSalary = new Empolyee();
+            Console.WriteLine($"{EmpolyeeSalary.name} Monthly wage = {EmpolyeeSalary.wage()}");
 
         }
     }
